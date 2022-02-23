fdaa0a6d4facf289d393196a76bb0a6655bd5d95
piepor <55095421+piepor@users.noreply.github.com>
2022-02-23 19:42:39 +0100

    played with road fine petri net. Add script to create and playout a simple data petri net

--- petri-net.py
+++ petri-net.py
@@ -1,4 +1,6 @@
 import pm4py
+import copy
+from random import choice
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.objects.petri_net.utils import petri_utils
@@ -9,6 +11,9 @@ from pm4py.algo.simulation.playout.petri_net import algorithm as simulator
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 from pm4py.algo.decision_mining import algorithm as decision_mining
 from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.objects.petri_net.data_petri_nets import semantics as dpn_semantics
+from pm4py.objects.petri_net import properties as petri_properties
+from pm4py.objects.petri_net.data_petri_nets.data_marking import DataMarking
 
 # create empty petri net
 net = PetriNet("petri_net")
@@ -61,4 +66,36 @@ log = pm4py.read_xes('roadtraffic100traces.xes')
 net, initial_marking, final_marking = inductive_miner.apply(log)
 gviz = pn_visualizer.apply(net, initial_marking, final_marking)
 pn_visualizer.view(gviz)
+pn_visualizer.save(gviz, "road-fine.svg")
 dpnet, im, fm = decision_mining.create_data_petri_nets_with_decisions(log, net, initial_marking, final_marking)
+
+#simulated_log_dpnet = simulator.apply(dpnet, im, variant=simulator.Variants.BASIC_PLAYOUT, parameters={
+#    simulator.Variants.BASIC_PLAYOUT.value.Parameters.NO_TRACES: 50, 
+#    simulator.Variants.BASIC_PLAYOUT.value.Parameters.PETRI_SEMANTICS: dpn_semantics.DataPetriNetSemantics})
+e = {"dismissal_A": 0.5, "amount": 25.0, "totalPaymentAmount": 33.0, "dismissal_NIL": 0.5, "article": 175.0, 
+        "vehicleClass_A": 0.5, "dismissal_I": 0.5, "points": 3.0}
+# transform initial marking in nm DataMarking for data petri net
+dm = DataMarking()
+dm[list(im.keys())[0]] = im.get(list(im.keys())[0])
+max_trace_length = 100
+#breakpoint()
+visited_elements = []
+while dm != fm and len(visited_elements) < max_trace_length:
+    #breakpoint()
+    all_enabled_trans = dpn_semantics.enabled_transitions(dpnet, dm, e)
+    print(all_enabled_trans)
+    for enabled in list(all_enabled_trans):
+        if "guard" in enabled.properties:
+            if not dpn_semantics.evaluate_guard(enabled.properties["guard"], enabled.properties["readVariable"], dm.data_dict):
+                all_enabled_trans.discard(enabled)
+    #breakpoint()
+    if len(all_enabled_trans) > 0:
+        trans = choice(list(all_enabled_trans))
+        dm = dpn_semantics.execute(trans, dpnet, dm, e)
+        print(dm)
+        visited_elements.append(trans)
+    else:
+        break
+            
+   
+
