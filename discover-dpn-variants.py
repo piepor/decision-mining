6955d7ff217d7892520d1a67691698603372d153
piepor <55095421+piepor@users.noreply.github.com>
2022-06-01 12:44:54 +0200

    Maybe the right time

--- discover-dpn-variants.py
+++ discover-dpn-variants.py
@@ -10,6 +10,7 @@ import copy
 #from sklearn.tree import DecisionTreeClassifier, export_text
 from pm4py.objects.petri_net.obj import Marking
 from sklearn import metrics
+from pm4py.algo.filtering.log.variants import variants_filter
 from pm4py.algo.decision_mining import algorithm as decision_mining
 from pm4py.objects.petri_net.importer import importer as pnml_importer
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
@@ -53,10 +54,6 @@ for trace in log:
                 event_attributes[attribute].append(event[attribute])
             else:
                 event_attributes[attribute] = [event[attribute]]
-#breakpoint()
-#df_trace_attr = pd.DataFrame.from_dict(trace_attributes)
-#df_event_attr = pd.DataFrame.from_dict(event_attributes)
-#breakpoint()
 net, im, fm = pm4py.discover_petri_net_inductive(log)
 sim_net = copy.deepcopy(net)
 sim_net, sim_map, last_collapsed_left, last_collapsed_right, parallel_branches = simplify_net(sim_net)
@@ -79,8 +76,8 @@ gviz = pn_visualizer.apply(net, im, fm)
 pn_visualizer.view(gviz)
 gviz = pn_visualizer.apply(sim_net, im_sim, fm_sim)
 pn_visualizer.view(gviz)
-#breakpoint()
-#pn_visualizer.save(gviz, "one-split-PetriNet-categorical.svg")
+# make sure that continuous data are float 
+# TODO do it following the conf file of the tree
 for trace in tqdm(log):
     for event in trace:
         for attr in event.keys():
@@ -93,10 +90,9 @@ for trace in tqdm(log):
 trans_events_map = get_map_transitions_events(net)
 events_trans_map = get_map_events_transitions(net)
 
+# detect loops and create loop objects
 loop_vertex = detect_loops(sim_net)
-#print(loop_vertex)
 loop_vertex = delete_composite_loops(loop_vertex)
-#breakpoint()
 
 loops = list()
 for loop_length in loop_vertex.keys():
@@ -108,10 +104,9 @@ for loop_length in loop_vertex.keys():
         loop_obj.set_complete_net_loop_nodes(sim_map)
         loops.append(loop_obj)
 
+# join different parts of the same loop (different loops with same inputs and outputs)
 not_joinable = False
-breakpoint()
 while not not_joinable:
-    #breakpoint()
     old_loops = copy.copy(loops)
     new_loop_nodes = None
     new_loop_events = None
@@ -139,27 +134,8 @@ while not not_joinable:
     if len(new_loop_nodes) == 0:
         not_joinable = True
             
-breakpoint()
 for loop in loops:
     loop.set_nearest_input_complete_net(sim_net, loops)
-#    # TODO: these functions have problems with road traffice fine management process complete dataset
-##    breakpoint()
-#    loop.set_dp_forward_order_transition(sim_net)
-##    breakpoint()
-#    loop.set_dp_backward_order_transition(sim_net)
-#prova_seq = ['Create Fine', 'Send Fine', 'Send Appeal to Prefecture', 'Insert Fine Notification']
-#prova_seq = ['Create Fine', 'Send Fine', 'Appeal to Judge']
-#prova_seq = ['Create Fine', 'Send Fine']
-#prova_seq_net_name = list()
-#for act_name in prova_seq:
-#    prova_seq_net_name.append(trans_events_map[act_name])
-#breakpoint()
-#bo = get_decision_points_and_targets(prova_seq_net_name, loops, net, parallel_branches)
-#    
-#breakpoint()
-
-# Get the map of places and events
-#general_places_events_map = get_map_place_to_events(net, loops)
 
 tic = time()
 
@@ -169,21 +145,49 @@ for place in net.places:
     if len(place.out_arcs) >= 2:
         decision_points_data[place.name] = {k: [] for k in ['target']}
 
-breakpoint()
+#breakpoint()
 event_attr = dict()
 
 # Scanning the log to get the data
 print('Extracting training data from Event Log')
-for trace in tqdm(log):
-    #places_events_map = general_places_events_map.copy()
-    #dp_list = list(places_events_map.keys())
+#for trace in tqdm(log):
+variants = variants_filter.get_variants(log)
+# searching only the variants
+for variant in tqdm(variants):
+    transitions_sequence = list()
+    events_sequence = list()
+    dp_events_sequence = dict()
+    for i, event_name in enumerate(variant.split(',')):
+        trans_from_event = trans_events_map[event_name]
+        transitions_sequence.append(trans_from_event)
+        events_sequence.append(event_name)
+        dp_dict = dict()
+        if len(transitions_sequence) > 1:
+            dp_dict = get_decision_points_and_targets(transitions_sequence, loops, net, parallel_branches)
+        old_keys = list(dp_dict.keys()).copy()
+        for dp in old_keys:
+            if len(dp_dict[dp]) == 0:
+                del dp_dict[dp]
+        if len(transitions_sequence) > 1:
+            print("Previous event: {}".format(events_sequence[-2]))
+            print("Current event: {}".format(events_sequence[-1]))
+            print("DPs")
+            for key in dp_dict.keys():
+                print(" - {}".format(key))
+                for inn_key in dp_dict[key]:
+                    if inn_key in events_trans_map.keys():
+                        print("   - {}".format(events_trans_map[inn_key]))
+                    else:
+                        print("   - {}".format(inn_key))
+            breakpoint()
+        dp_events_sequence['Event_{}'.format(i+1)] = dp_dict
+    # Final update of the current trace (from last event to sink)
+    transition = [trans for trans in net.transitions if trans.label == event_name][0]
+
+    places_from_event = get_all_dp_from_event_to_sink(transition, loops, dict(), set())
+    dp_events_sequence['End'] = places_from_event
     transition_sequence = list()
     event_sequence = list()
-    #number_of_loops = dict()
-    #for loop in loops:
-    #    loop.set_inactive()
-    #    number_of_loops[loop.name] = 0
-
     # Keep the same attributes observed in the previous traces (to keep dictionaries at the same length)
     event_attr = {k: [np.nan] for k in event_attr.keys()}
     # Store the trace attributes (if any)
@@ -191,76 +195,52 @@ for trace in tqdm(log):
         event_attr.update(trace.attributes)
     last_event_name = 'None'
 
-    for event in trace:
-        trans_from_event = trans_events_map[event["concept:name"]]
-        transition_sequence.append(trans_from_event)
-        event_sequence.append(event['concept:name'])
-        #breakpoint()
-        if len(transition_sequence) > 1:
-            dp_dict = get_decision_points_and_targets(transition_sequence, loops, net, parallel_branches)
-            #breakpoint()
-            #last_event_trans = trans_events_map[last_event_name]
+    for trace in variants[variant]:
+        for i, event in enumerate(trace):
+            trans_from_event = trans_events_map[event["concept:name"]]
+            transition_sequence.append(trans_from_event)
+            event_sequence.append(event['concept:name'])
+            if len(transition_sequence) > 1:
+                dp_dict = dp_events_sequence['Event_{}'.format(i+1)]
+                for dp in dp_dict.keys():
+                    # Append the last attribute values to the decision point dictionary
+                    for dp_target in dp_dict[dp]:
+                        for a in event_attr.keys():
+                            # If attribute is not present, and it is not nan, add it as a new key, filling previous entries with nan
+                            #if a not in decision_points_data[place_from_event[0]] and event_attr[a][0] is not np.nan:
+                            if a not in decision_points_data[dp] and event_attr[a][0] is not np.nan:
+                                entries = len(decision_points_data[dp]['target'])
+                                #entries = len(decision_points_data[place_from_event[0]]['target'])
+                                #decision_points_data[place_from_event[0]][a] = [np.nan] * entries
+                                decision_points_data[dp][a] = [np.nan] * entries
+                                #decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
+                                decision_points_data[dp][a].append(event_attr[a][0])
+                            # Else, if attribute is present, just append it to the existing list
+                            #elif a in decision_points_data[place_from_event[0]]:
+                            elif a in decision_points_data[dp]:
+                                #decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
+                                decision_points_data[dp][a].append(event_attr[a][0])
+                        # Append also the target transition label to the decision point dictionary
+                        decision_points_data[dp]['target'].append(dp_target)
 
-            #places_events_map, dp_list = update_places_map_dp_list_if_looping(net, dp_list, places_events_map, loops, event_sequence, number_of_loops, trans_events_map)
-            #places_from_event = get_place_from_event(places_events_map, event['concept:name'], dp_list)
-            #dp_list = update_dp_list(places_from_event, dp_list)
+            # Get the attribute values dictionary containing the current event values
+            event_attr.update(get_attributes_from_event(event))
+            [event_attr.pop(k) for k in ['time:timestamp', 'concept:name']]
+            # Update the last event name with the current event name
+            last_event_name = event['concept:name']
 
-            #for place_from_event in places_from_event:
-            for dp in dp_dict.keys():
-                # Append the last attribute values to the decision point dictionary
-                for dp_target in dp_dict[dp]:
+        places_from_event = dp_events_sequence['End']
+        if len(places_from_event) > 0:
+            for place_from_event in places_from_event.keys():
+                for target_act in places_from_event[place_from_event]:
                     for a in event_attr.keys():
-                        # If attribute is not present, and it is not nan, add it as a new key, filling previous entries with nan
-                        #if a not in decision_points_data[place_from_event[0]] and event_attr[a][0] is not np.nan:
-                        if a not in decision_points_data[dp] and event_attr[a][0] is not np.nan:
-                            entries = len(decision_points_data[dp]['target'])
-                            #entries = len(decision_points_data[place_from_event[0]]['target'])
-                            #decision_points_data[place_from_event[0]][a] = [np.nan] * entries
-                            decision_points_data[dp][a] = [np.nan] * entries
-                            #decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
-                            decision_points_data[dp][a].append(event_attr[a][0])
-                        # Else, if attribute is present, just append it to the existing list
-                        #elif a in decision_points_data[place_from_event[0]]:
-                        elif a in decision_points_data[dp]:
-                            #decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
-                            decision_points_data[dp][a].append(event_attr[a][0])
-                    # Append also the target transition label to the decision point dictionary
-                    decision_points_data[dp]['target'].append(dp_target)
-
-        # Get the attribute values dictionary containing the current event values
-        event_attr.update(get_attributes_from_event(event))
-        [event_attr.pop(k) for k in ['time:timestamp', 'concept:name']]
-        # Update the last event name with the current event name
-        last_event_name = event['concept:name']
-
-    # Final update of the current trace (from last event to sink)
-    transition = [trans for trans in net.transitions if trans.label == event['concept:name']][0]
-
-    places_from_event = get_all_dp_from_event_to_sink(transition, loops, dict(), set())
-    #breakpoint()
-
-#    if len(places_from_event) > 0:
-#        for place_from_event in places_from_event:
-#            for a in event_attr.keys():
-#                if a not in decision_points_data[place_from_event[0]] and event_attr[a][0] is not np.nan:
-#                    entries = len(decision_points_data[place_from_event[0]]['target'])
-#                    decision_points_data[place_from_event[0]][a] = [np.nan] * entries
-#                    decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
-#                elif a in decision_points_data[place_from_event[0]]:
-#                    decision_points_data[place_from_event[0]][a].append(event_attr[a][0])
-#            decision_points_data[place_from_event[0]]['target'].append(place_from_event[1])
-
-    if len(places_from_event) > 0:
-        for place_from_event in places_from_event.keys():
-            for target_act in places_from_event[place_from_event]:
-                for a in event_attr.keys():
-                    if a not in decision_points_data[place_from_event] and event_attr[a][0] is not np.nan:
-                        entries = len(decision_points_data[place_from_event]['target'])
-                        decision_points_data[place_from_event][a] = [np.nan] * entries
-                        decision_points_data[place_from_event][a].append(event_attr[a][0])
-                    elif a in decision_points_data[place_from_event]:
-                        decision_points_data[place_from_event][a].append(event_attr[a][0])
-                decision_points_data[place_from_event]['target'].append(target_act)
+                        if a not in decision_points_data[place_from_event] and event_attr[a][0] is not np.nan:
+                            entries = len(decision_points_data[place_from_event]['target'])
+                            decision_points_data[place_from_event][a] = [np.nan] * entries
+                            decision_points_data[place_from_event][a].append(event_attr[a][0])
+                        elif a in decision_points_data[place_from_event]:
+                            decision_points_data[place_from_event][a].append(event_attr[a][0])
+                    decision_points_data[place_from_event]['target'].append(target_act)
 
 #breakpoint()
 #attributes_map = {'lifecycle.transition': 'categorical', 'expense': 'continuous',
