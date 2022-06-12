0917736644569ac68e221c0a23c5361cf6f4f99b
piepor <55095421+piepor@users.noreply.github.com>
2022-06-12 13:54:13 +0200

    create and use the configuration file for dt

--- create-ds-conifg.py
+++ create-ds-conifg.py
@@ -56,7 +56,7 @@ def analyse_ds(_log):
 
     return df_trace, df, unique_values, unique_values_trace
 
-def save_json(dict_conf, data_dir='data'):
+def save_json(dict_conf, data_dir='dt-attributes'):
     json_string = json.dumps(dict_conf)
     #breakpoint()
     file_name = '{}.attr'.format(st.session_state.uploaded_file.split('log-')[1].split('.xes')[0])
@@ -67,7 +67,7 @@ def create_dict(st_session_state):
     dict_conf = dict()
     #breakpoint()
     for name in st_session_state:
-        if 'e_' in name or 't_' in name:
+        if ('e_' in name or 't_' in name) and name.split('_')[1] not in ['trace', 'event']:
             dict_conf[name.split('_')[1]] = st.session_state[name]
     return dict_conf
 
